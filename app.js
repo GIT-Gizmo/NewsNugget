c4f38a139c57c2cbffff4ce0b904a0c91f2bf023
GIT-Gizmo <boluforjesus@gmail.com>
2024-03-08 09:54:49 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,81 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+const { send } = require("process");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server is running on ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const apiKey = process.env.API_KEY;
+  const audienceId = process.env.MAILCHIMP_LIST_ID;
+  const firstName = req.body.firstname;
+  const lastName = req.body.lastname;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = `https://us18.api.mailchimp.com/3.0/lists/${audienceId}`;
+
+  const options = {
+    method: "POST",
+    auth: `bolu1:${apiKey}`,
+  };
+
+  const request = https.request(url, options, (response) => {
+    try {
+        // const dataInfo = JSON.parse(data);
+        // const subscribed = dataInfo.new_members[0].status;
+      response.on("data", (data) => {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/error.html")
+        }
+      });
+    } catch (error) {
+        response.on("error", (error) => {
+            console.error(error, response.statusCode);
+            res.sendFile(__dirname + "/error.html");
+        })
+    }
+  });
+
+//   request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", (req, res) => {
+    res.redirect("/");
+})
