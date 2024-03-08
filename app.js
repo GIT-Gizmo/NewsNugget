79242c530bfd63028119a9b4fbde999e64e83225
GIT-Gizmo <boluforjesus@gmail.com>
2024-03-08 11:18:31 +0100

    Changed static file location

--- app.js
+++ app.js
@@ -89,7 +89,10 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+// Serve static files from the root directory and the public directory
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(__dirname)); // Serve static files from the root directory
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(process.env.PORT || port, () => {
